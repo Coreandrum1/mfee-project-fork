b18ec9f0baa65b3dad1fdaeea51fb241e91a7669
Coreandrum1 <core_andrum769@hotmail.com>
2024-09-16 16:42:29 -0700

    info comments

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,12 +9,12 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
 
-app.use(express.json());
-app.use(helmet());
-app.use(cors(corsOptions));
+app.use(express.json()); // middleware to parse json
+app.use(helmet()); // middleware to secure the app / remove x-powered-by
+app.use(cors(corsOptions)); // middleware to enable cors
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello MFEE!' });
+  res.send({ message: 'Hello MFEE! 22' });
 });
 
 app.listen(port, host, () => {
